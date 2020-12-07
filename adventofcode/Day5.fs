527b5fe63890338f8b88e55596c737319c41bee2
dawe <dawedawe@gmx.de>
2020-12-07 10:45:26 +0100

    Day 5 Part 1

---
+++ adventofcode/Day5.fs
@@ -0,0 +1,28 @@
+namespace Adventofcode
+
+module Day5 =
+
+    [<Literal>]
+    let InputFile = "Day5Input.txt"
+
+    let getHalf lowerBound upperBound half =
+        match half with
+        | x when x = 'F' || x = 'L' -> lowerBound, lowerBound + ((upperBound - lowerBound) / 2)
+        | x when x = 'B' || x = 'R' -> lowerBound + ((upperBound - lowerBound) / 2) + 1, upperBound
+        | _ -> failwith "unsupported partitioning"
+
+    let calculateSeatId (s : string) =
+        let partitions = s.ToCharArray()
+        let rec helper parts lower upper =
+            match parts with
+            | [||] -> lower
+            | ps -> let lower', upper' = getHalf lower upper ps.[0]
+                    helper ps.[1..] lower' upper'
+        let row = helper partitions.[0 .. 6] 0 127
+        let col = helper partitions.[7 ..] 0 7
+        row * 8 + col
+
+    let day5 () =
+        System.IO.File.ReadAllLines InputFile
+        |> Array.map calculateSeatId
+        |> Array.max
