4262b1d81dd4e85f70e51e58407b8a036e2ca91b
dawe <dawedawe@gmx.de>
2020-12-07 12:14:46 +0100

    Day 6 Part 2

--- adventofcode/Day6.fs
+++ adventofcode/Day6.fs
@@ -26,3 +26,24 @@ module Day6 =
         System.IO.File.ReadAllLines InputFile
         |> parse
         |> Array.sumBy count
+
+    let parsePart2 lines =
+        let rec helper (sofar: string [] []) (lines: string []) =
+            if lines = Array.empty then
+                sofar
+            else
+                let groupLines = Array.takeWhile (fun s -> s <> "") lines
+                let sofar' = Array.append sofar [| groupLines |]
+                let lines' = lines.[(groupLines.Length + 1)..]
+                helper sofar' lines'
+        helper Array.empty lines
+
+    let countPart2 (group: string []) =
+        let charArrays = Array.map (fun (s: string) -> s.ToCharArray() |> Set.ofArray) group
+        let i = Array.fold Set.intersect charArrays.[0] charArrays
+        Set.count i
+
+    let day6Part2() =
+        System.IO.File.ReadAllLines InputFile
+        |> parsePart2
+        |> Array.sumBy countPart2
