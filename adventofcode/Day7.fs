e729710b8878cb852caf78f3f91d3513f2ebe08d
dawe <dawedawe@gmx.de>
2020-12-07 18:22:08 +0100

    Day 7 Part 2

--- adventofcode/Day7.fs
+++ adventofcode/Day7.fs
@@ -7,23 +7,27 @@ module Day7 =
 
     type Relation =
         { Outer: string
+          InnerCount: int
           Inner: Option<string> }
 
     let parseInners (line: string) =
         let index = line.IndexOf("contain ")
-        line.Substring(index + 8).Split(", ") |> Array.map ((fun s -> s.Split(" ")) >> (fun a -> a.[1] + " " + a.[2]))
+        line.Substring(index + 8).Split(", ")
+        |> Array.map ((fun s -> s.Split(" ")) >> (fun a -> int a.[0],  a.[1] + " " + a.[2]))
 
     let parse (s: string) =
         let words = s.Split(' ')
         let outer = words.[0] + " " + words.[1]
         if (s.EndsWith("contain no other bags.")) then
             [| { Outer = outer
-                 Inner = None } |]
+                 Inner = None
+                 InnerCount = 0 } |]
         else
             parseInners s
             |> Array.map (fun inner ->
                 { Outer = outer
-                  Inner = Some inner })
+                  InnerCount = fst inner
+                  Inner = Some (snd inner) })
 
     let rec getRelations relations inner =
         let containingRelations = Array.filter (fun r -> r.Inner = Some inner) relations
@@ -42,3 +46,20 @@ module Day7 =
             |> Array.map (fun r -> r.Outer)
             |> Set.ofArray
         Set.count outers
+
+    let getInnerBags relations bag =
+        let rec helper relations b factor =
+            let containedBags = Array.filter (fun r -> r.Outer = b) relations
+            if (containedBags = Array.empty)
+            then 0
+            else
+                let deeper = Array.filter (fun r -> r.InnerCount > 0) containedBags
+                let nextRelations = Array.map (fun r -> helper relations r.Inner.Value r.InnerCount) deeper
+                factor + Array.sumBy (fun r -> factor * r) nextRelations
+        (helper relations bag 1) - 1
+
+    let day7Part2() =
+        let lines = InputFile |> System.IO.File.ReadAllLines
+        let relations = Array.collect parse lines
+        let bags = getInnerBags relations "shiny gold"
+        bags
