4b002c0e9a4860523f48db122291f8a19748776d
dawe <dawedawe@gmx.de>
2020-12-08 18:12:03 +0100

    Day 8 Part 2

--- adventofcode/Day8.fs
+++ adventofcode/Day8.fs
@@ -8,7 +8,7 @@ module Day8 =
     type Instruction =
         | Acc of int
         | Jmp of int
-        | Nop
+        | Nop of int
 
     let parse (line: string) =
         let words = line.Split(" ")
@@ -16,7 +16,7 @@ module Day8 =
         match words.[0] with
         | "acc" -> Acc arg
         | "jmp" -> Jmp arg
-        | "nop" -> Nop
+        | "nop" -> Nop arg
         | _ -> failwith "unsupported instruction"
 
     type State =
@@ -48,12 +48,55 @@ module Day8 =
                     match code.[state.InstPointer] with
                     | Acc a -> acc state a
                     | Jmp a -> jmp state a
-                    | Nop -> nop state
+                    | Nop _ -> nop state
                 helper state' executed'
 
         let initialState = init()
         helper initialState Set.empty
 
     let day8() =
-        let code = System.IO.File.ReadAllLines InputFile |> Array.map parse
-        run code
+        System.IO.File.ReadAllLines InputFile
+        |> Array.map parse
+        |> run
+
+    let runPart2 (code: Instruction []) =
+        let rec helper (state: State) (executed: Set<int>) =
+            if (executed.Contains state.InstPointer) then
+                None
+            else if state.InstPointer = code.Length then
+                Some state.Accumulator
+            else
+                let executed' = Set.add state.InstPointer executed
+
+                let state' =
+                    match code.[state.InstPointer] with
+                    | Acc a -> acc state a
+                    | Jmp a -> jmp state a
+                    | Nop _ -> nop state
+                helper state' executed'
+
+        let initialState = init()
+        helper initialState Set.empty
+
+    let changeInstruction (i: int) (code: Instruction []) =
+        let code' = Array.copy code
+        let instr = code'.[i]
+        match instr with
+        | Jmp x -> code'.[i] <- Nop x
+        | Nop x -> code'.[i] <- Jmp x
+        | _ -> ()
+        code'
+
+    let rec findInstruction code =
+        let rec helper i =
+            let code' = changeInstruction i code
+            let result = runPart2 code'
+            match result with
+            | None -> helper (i + 1)
+            | Some v -> v
+        helper 0
+
+    let day8Part2() =
+        System.IO.File.ReadAllLines InputFile
+        |> Array.map parse
+        |> findInstruction
