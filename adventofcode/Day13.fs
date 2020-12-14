c3a4722f543b71e84d4a8571a7b801cf8e5758d2
dawe <dawedawe@gmx.de>
2020-12-14 12:18:25 +0100

    Day 13 Part 2

--- adventofcode/Day13.fs
+++ adventofcode/Day13.fs
@@ -2,6 +2,8 @@ namespace Adventofcode
 
 module Day13 =
 
+    open System.Linq
+
     [<Literal>]
     let InputFile = "Day13Input.txt"
 
@@ -10,9 +12,34 @@ module Day13 =
     let day13() =
         let lines = System.IO.File.ReadAllLines InputFile
         let timestamp = int lines.[0]
-        let busses = lines.[1]
-                     |> fun (s: string) -> s.Split(",")
-                     |> Array.filter (fun c -> c <> "x")
-                     |> Array.map (int >> (fun b -> b, (f timestamp b)))
-                     |> Array.sortBy snd
+
+        let busses =
+            lines.[1]
+            |> fun (s: string) -> s.Split(",")
+            |> Array.filter (fun c -> c <> "x")
+            |> Array.map (int >> (fun b -> b, (f timestamp b)))
+            |> Array.sortBy snd
         fst busses.[0] * snd busses.[0]
+
+    let f2 (t: int64) (busId: int64) (busIndex: int64) =
+        let tOfBus = t + busIndex
+        (tOfBus % busId) = 0L
+
+    let day13Part2() =
+        let input = System.IO.File.ReadAllLines InputFile
+                    |> Array.item 1
+                    |> fun (s: string) -> s.Split(",")
+                    |> List.ofArray
+        let mutable busses =
+            List.zip input [ 0L .. (int64 input.Length - 1L) ]
+            |> List.filter (fun x -> fst x <> "x")
+            |> List.map (fun (bus, index) -> (int64 bus, index))
+        let mutable timestamp = 0L
+        let mutable step = 1L
+
+        while busses.Length > 0 do
+            if (f2 timestamp (fst busses.[0]) (snd busses.[0]))
+            then step <- step * fst busses.[0]
+                 busses <- busses.[1..]
+            else timestamp <- timestamp + step
+        timestamp
