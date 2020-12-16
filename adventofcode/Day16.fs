4b6bfb31b16bb25f85e48b36109dd1edcf5c1f6f
dawe <dawedawe@gmx.de>
2020-12-16 12:56:15 +0100

    Day 16 Part 2

--- adventofcode/Day16.fs
+++ adventofcode/Day16.fs
@@ -25,8 +25,13 @@ module Day16 =
         | _ ->
             let rule = rules.[0]
 
-            let valid = (fst rule.Range1) <= field && field <= (snd rule.Range1)
-                        || (fst rule.Range2) <= field && field <= (snd rule.Range2)
+            let valid =
+                (fst rule.Range1)
+                <= field
+                && field <= (snd rule.Range1)
+                || (fst rule.Range2)
+                   <= field
+                   && field <= (snd rule.Range2)
 
             if valid then 0 else validateField rules.[1..] field
 
@@ -36,9 +41,105 @@ module Day16 =
         let lines = System.IO.File.ReadAllLines InputFile
         let ruleLines = Array.takeWhile ((<>) "") lines
 
-        let nearbyTicketsLines =
+        let nearbyTickets =
             lines.[ruleLines.Length + 5..]
             |> Array.map (fun s -> s.Split(",") |> Array.map int)
 
         let rules = Array.map parseRule ruleLines
-        nearbyTicketsLines |> Array.sumBy (validate rules)
+        nearbyTickets |> Array.sumBy (validate rules)
+
+    let rec validateRule (values: int []) (rule: Rule) =
+        match values with
+        | [||] -> true
+        | _ ->
+            let value = values.[0]
+
+            let valid =
+                (fst rule.Range1)
+                <= value
+                && value <= (snd rule.Range1)
+                || (fst rule.Range2)
+                   <= value
+                   && value <= (snd rule.Range2)
+
+            if valid then validateRule values.[1..] rule else false
+
+    let findMatchingRules (rules: Rule []) (tickets: int [] []) =
+        let helper (fieldValues: int []) =
+            Array.filter (validateRule fieldValues) rules
+
+        seq {
+            for i in [ 0 .. tickets.[0].Length - 1 ] do
+                let iValues = tickets |> Array.map (fun a -> a.[i])
+                yield (i, helper iValues)
+        }
+
+    let findFieldOrder (rules: Rule []) (tickets: int [] []) =
+        let rec helper (fieldsAndRules: Set<int * (Rule [])>) =
+
+            let mutable finalMatchings =
+                Set.filter (fun (_, x) -> Array.length x = 1) fieldsAndRules
+
+            let mutable undecidedMatchings =
+                Set.difference fieldsAndRules finalMatchings
+
+            for (_, finalMatching) in finalMatchings do
+                let rulesToTrim =
+                    Set.filter (fun (_, rs) ->
+                        (Array.tryFind (fun r -> r.Name = finalMatching.[0].Name) rs)
+                        <> None) undecidedMatchings
+
+                for (i, rs) in rulesToTrim do
+                    let trimmmed =
+                        Array.filter (fun r -> r.Name <> finalMatching.[0].Name) rs
+
+                    undecidedMatchings <- undecidedMatchings.Remove(i, rs)
+                    undecidedMatchings <- undecidedMatchings.Add(i, trimmmed)
+
+            let u =
+                Set.union finalMatchings undecidedMatchings
+
+            let undecided =
+                Set.filter (fun (_, x) -> Array.length x > 1) u
+                |> Set.count
+
+            if (undecided > 0) then helper u else u
+
+
+        let fieldsAndMatchingRules =
+            findMatchingRules rules tickets |> Set.ofSeq
+
+        helper fieldsAndMatchingRules
+
+    let day16Part2 () =
+        let lines = System.IO.File.ReadAllLines InputFile
+        let ruleLines = Array.takeWhile ((<>) "") lines
+
+        let myTicket =
+            lines.[ruleLines.Length + 2]
+            |> fun s -> s.Split(",")
+            |> Array.map int
+
+        let nearbyTickets =
+            lines.[ruleLines.Length + 5..]
+            |> Array.map (fun s -> s.Split(",") |> Array.map int)
+
+        let rules = Array.map parseRule ruleLines
+
+        let validTickets =
+            nearbyTickets
+            |> Array.filter (fun t -> validate rules t = 0)
+
+        let indexes =
+            findFieldOrder rules validTickets
+            |> Seq.filter (fun (i, r) -> r.[0].Name.StartsWith("departure"))
+            |> Seq.map fst
+
+        let product =
+            seq {
+                for i in indexes do
+                    yield int64 myTicket.[i]
+            }
+            |> Seq.fold (*) 1L
+
+        product
