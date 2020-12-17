56411a5339b61e093c13bbecb0e5b06a2cd0a97d
dawe <dawedawe@gmx.de>
2020-12-17 14:43:27 +0100

    Day 17 Part 2

--- adventofcode/Day17.fs
+++ adventofcode/Day17.fs
@@ -15,7 +15,7 @@ module Day17 =
                 space <- Map.add (x, y, 0) a.[x].[y] space
         space
 
-    let copySpace (space: Map<(int * int * int), char>) =
+    let copySpace (space: Map<'a, 'b>) =
         let mutable copy = Map.empty
         for item in space do
             copy <- Map.add item.Key item.Value space
@@ -59,8 +59,8 @@ module Day17 =
                         if ((xDiff, yDiff, zDiff) <> (0, 0, 0)) then (x + xDiff, y + yDiff, z + zDiff)
         }
 
-    let countActiveNeighbours (x, y, z) (space: Map<(int * int * int), char>) =
-        let positions = getNeighbourPositions (x, y, z)
+    let countActiveNeighbours (key: 'a) posFunc (space: Map<'a, char>) =
+        let positions = posFunc key
         seq {
             for p in positions do
                 if (Map.containsKey p space && space.[p] = '#')
@@ -68,10 +68,12 @@ module Day17 =
         }
         |> Seq.length
 
-    let cycle space =
-        let mutable spaceGrown = grow space
+    let cycle (growFunc: Map<'a, char> -> Map<'a, char>) (posFunc: 'a -> seq<'a>) space =
+        let mutable spaceGrown = growFunc space
         for kv in spaceGrown do
-            let activeNeighbours = countActiveNeighbours kv.Key space
+            let activeNeighbours =
+                countActiveNeighbours kv.Key posFunc space
+
             match (activeNeighbours, kv.Value) with
             | (2, '#') -> spaceGrown <- Map.add kv.Key '#' spaceGrown
             | (3, '#') -> spaceGrown <- Map.add kv.Key '#' spaceGrown
@@ -79,14 +81,14 @@ module Day17 =
             | _ -> spaceGrown <- Map.add kv.Key '.' spaceGrown
         spaceGrown
 
-    let rec doCycles (space: Map<(int * int * int), char>) n =
+    let rec doCycles cycleFunc (space: Map<'a, char>) n =
         if n = 0 then
             space
         else
-            let space' = cycle space
-            doCycles space' (n - 1)
+            let space' = cycleFunc space
+            doCycles cycleFunc space' (n - 1)
 
-    let count (space: Map<(int * int * int), char>) =
+    let count (space: Map<'a, char>) =
         space
         |> Map.filter (fun _ v -> v = '#')
         |> Map.count
@@ -94,4 +96,82 @@ module Day17 =
     let day17 () =
         let lines = System.IO.File.ReadAllLines InputFile
         let space = init lines
-        doCycles space 6 |> count
+        doCycles (cycle grow getNeighbourPositions) space 6
+        |> count
+
+    let initPart2 lines =
+        let a =
+            Array.map (fun (s: string) -> s.ToCharArray()) lines
+
+        let mutable space = Map.empty
+        for x in [ 0 .. a.Length - 1 ] do
+            for y in [ 0 .. a.[0].Length - 1 ] do
+                space <- Map.add (x, y, 0, 0) a.[x].[y] space
+        space
+
+    let growPart2 (space: Map<(int * int * int * int), char>) =
+        let mutable space' = copySpace space
+        let spaceA = Map.toArray space |> Array.map fst
+
+        let xSorted =
+            Array.sortBy (fun (x, _, _, _) -> x) spaceA
+
+        let ySorted =
+            Array.sortBy (fun (_, y, _, _) -> y) spaceA
+
+        let zSorted =
+            Array.sortBy (fun (_, _, z, _) -> z) spaceA
+
+        let wSorted =
+            Array.sortBy (fun (_, _, _, w) -> w) spaceA
+
+        let (minX, _, _, _), (maxX, _, _, _) = Array.head xSorted, Array.last xSorted
+        let (minX', maxX') = (minX - 1, maxX + 1)
+        let (_, minY, _, _), (_, maxY, _, _) = Array.head ySorted, Array.last ySorted
+        let (minY', maxY') = (minY - 1, maxY + 1)
+        let (_, _, minZ, _), (_, _, maxZ, _) = Array.head zSorted, Array.last zSorted
+        let (minZ', maxZ') = (minZ - 1, maxZ + 1)
+        let (_, _, _, minW), (_, _, _, maxW) = Array.head wSorted, Array.last wSorted
+        let (minW', maxW') = (minW - 1, maxW + 1)
+
+        for y in [ minY' .. maxY' ] do
+            for z in [ minZ' .. maxZ' ] do
+                for w in [ minW' .. maxW' ] do
+                    space' <- Map.add (minX', y, z, w) '.' space'
+                    space' <- Map.add (maxX', y, z, w) '.' space'
+
+        for x in [ minX' .. maxX' ] do
+            for z in [ minZ' .. maxZ' ] do
+                for w in [ minW' .. maxW' ] do
+                    space' <- Map.add (x, minY', z, w) '.' space'
+                    space' <- Map.add (x, maxY', z, w) '.' space'
+
+        for x in [ minX' .. maxX' ] do
+            for y in [ minY' .. maxY' ] do
+                for w in [ minW' .. maxW' ] do
+                    space' <- Map.add (x, y, minZ', w) '.' space'
+                    space' <- Map.add (x, y, maxZ', w) '.' space'
+
+        for x in [ minX' .. maxX' ] do
+            for y in [ minY' .. maxY' ] do
+                for z in [ minZ' .. maxZ' ] do
+                    space' <- Map.add (x, y, z, minW') '.' space'
+                    space' <- Map.add (x, y, z, maxW') '.' space'
+
+        space'
+
+    let getNeighbourPositionsPart2 (x, y, z, w) =
+        seq {
+            for xDiff in [ -1 .. 1 ] do
+                for yDiff in [ -1 .. 1 ] do
+                    for zDiff in [ -1 .. 1 ] do
+                        for wDiff in [ -1 .. 1 ] do
+                            if ((xDiff, yDiff, zDiff, wDiff) <> (0, 0, 0, 0))
+                            then (x + xDiff, y + yDiff, z + zDiff, w + wDiff)
+        }
+
+    let day17Part2 () =
+        let lines = System.IO.File.ReadAllLines InputFile
+        let space = initPart2 lines
+        doCycles (cycle growPart2 getNeighbourPositionsPart2) space 6
+        |> count
