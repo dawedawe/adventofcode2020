44b8baa8353657d683ba07dfbd8a7bca7c4a29af
dawe <dawedawe@gmx.de>
2021-01-09 10:29:59 +0100

    Day 22 Part 2

--- adventofcode/Day22.fs
+++ adventofcode/Day22.fs
@@ -19,23 +19,66 @@ module Day22 =
 
         (deck1, deck2)
 
-    let play (deck1: int list) (deck2: int list) =
-        let (top1, top2) = (deck1.[0], deck2.[0])
-
+    let decideOnTop (top1: int) (top2: int) (deck1Rest: int list) (deck2Rest: int list) =
         if top1 > top2 then
-            (deck1.[1..] @ [ top1; top2 ], deck2.[1..])
+            (deck1Rest @ [ top1; top2 ], deck2Rest)
         else
-            (deck1.[1..], deck2.[1..] @ [ top2; top1 ])
+            (deck1Rest, deck2Rest @ [ top2; top1 ])
 
     let rec game ((deck1, deck2): int list * int list) =
         match (deck1, deck2) with
         | _, [] -> deck1
         | [], _ -> deck2
-        | _, _ -> play deck1 deck2 |> game
+        | _, _ ->
+            decideOnTop deck1.[0] deck2.[0] deck1.[1..] deck2.[1..]
+            |> game
+
+    let deckScore deck =
+        deck
+        |> List.rev
+        |> List.zip [ 1 .. deck.Length ]
+        |> List.sumBy (fun (a, b) -> a * b)
 
     let day22 () =
-        let lines = System.IO.File.ReadAllLines InputFile
-        let (deck1, deck2) = parse lines
-        let winner = game (deck1, deck2)
-        let indexes = [1 .. winner.Length]
-        winner |> List.rev |> List.zip indexes |> List.sumBy (fun (a, b) -> a * b)
+        InputFile
+        |> System.IO.File.ReadAllLines
+        |> parse
+        |> game
+        |> deckScore
+
+    let rec gamePart2 (decks: int list * int list) =
+        let mutable states = Set.empty<int list * int list>
+
+        let rec playRounds ((deck1, deck2): int list * int list) =
+            if states.Contains(deck1, deck2) then
+                (1, deck1)
+            else
+                states <- Set.add (deck1, deck2) states
+
+                match (deck1, deck2) with
+                | _, [] -> (1, deck1)
+                | [], _ -> (2, deck2)
+                | _, _ ->
+                    let (top1, top2) = (deck1.[0], deck2.[0])
+                    let (deck1', deck2') = (deck1.[1..], deck2.[1..])
+
+                    if deck1'.Length >= top1 && deck2'.Length >= top2 then
+                        let (winner, _) =
+                            gamePart2 (List.take top1 deck1', List.take top2 deck2')
+
+                        if winner = 1 then
+                            playRounds (deck1' @ [ top1; top2 ], deck2')
+                        else
+                            playRounds (deck1', deck2' @ [ top2; top1 ])
+                    else
+                        decideOnTop top1 top2 deck1' deck2' |> playRounds
+
+        playRounds decks
+
+    let day22Part2 () =
+        InputFile
+        |> System.IO.File.ReadAllLines
+        |> parse
+        |> gamePart2
+        |> snd
+        |> deckScore
