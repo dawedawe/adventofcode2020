fc14a8e7bc229af0a07f9cbec58d3d8a31d27caa
dawe <dawedawe@gmx.de>
2021-01-15 23:49:16 +0100

    Day 24 Part 2

--- adventofcode/Day24.fs
+++ adventofcode/Day24.fs
@@ -63,3 +63,92 @@ module Day24 =
             |> tile
 
         floor.Values.Where(id).Count()
+
+    let fillFloor (floor: Dictionary<int * int, bool>) =
+        let xPositions = floor.Keys.Select(fst).ToList()
+        let minX = xPositions.Min()
+        let maxX = xPositions.Max()
+        let yPositions = floor.Keys.Select(snd).ToList()
+        let minY = yPositions.Min()
+        let maxY = yPositions.Max()
+
+        for x in minX .. maxX do
+            for y in minY .. maxY do
+                if not (floor.ContainsKey(x, y)) then
+                    floor.[(x, y)] <- false
+
+    let addBorders (floor: Dictionary<int * int, bool>) =
+        let xPositions = floor.Keys.Select(fst).ToList()
+        let minX = xPositions.Min() - 1
+        let maxX = xPositions.Max() + 1
+        let yPositions = floor.Keys.Select(snd).ToList()
+        let minY = yPositions.Min() - 1
+        let maxY = yPositions.Max() + 1
+
+        for x in [ minX; maxX ] do
+            for y in [ minY .. maxY ] do
+                if not (floor.ContainsKey(x, y)) then
+                    floor.[(x, y)] <- false
+
+        for y in [ minY; maxY ] do
+            for x in [ minX .. maxX ] do
+                if not (floor.ContainsKey(x, y)) then
+                    floor.[(x, y)] <- false
+
+    let copyFloor (currentFloor: Dictionary<int * int, bool>) =
+        let copy = Dictionary<int * int, bool>()
+
+        for kv in currentFloor do
+            copy.Add(kv.Key, kv.Value)
+
+        copy
+
+    let countBlackNeighbours (floor: Dictionary<int * int, bool>) p =
+        let neighbourPos =
+            [ move p NorthEast
+              move p East
+              move p SouthEast
+              move p SouthWest
+              move p West
+              move p NorthWest ]
+
+        seq {
+            for n in neighbourPos do
+                if floor.ContainsKey(n) && floor.[n] then
+                    yield 1
+        }
+        |> Seq.sum
+
+    let trans (floor: Dictionary<int * int, bool>) p =
+        let blackNeighbours = countBlackNeighbours floor p
+
+        match floor.[p], blackNeighbours with
+        | true, n when n = 0 || n > 2 -> false
+        | false, 2 -> true
+        | _ -> floor.[p]
+
+    let processDays (floor: Dictionary<int * int, bool>) days =
+        let rec helper n (currentFloor: Dictionary<int * int, bool>) =
+            if n = 0 then
+                currentFloor
+            else
+                addBorders currentFloor
+                let nextFloor = copyFloor currentFloor
+
+                for p in currentFloor.Keys do
+                    nextFloor.[p] <- trans currentFloor p
+
+                helper (n - 1) nextFloor
+
+        helper days floor
+
+    let day24Part2 () =
+        let floor =
+            InputFile
+            |> System.IO.File.ReadAllLines
+            |> Array.map parseLine
+            |> tile
+
+        fillFloor floor
+        let floor' = processDays floor 100
+        floor'.Values.Where(id).Count()
